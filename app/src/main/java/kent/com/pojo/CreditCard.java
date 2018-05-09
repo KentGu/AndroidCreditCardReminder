ed17cce434f4960004a7b955de4503042dc86957
KentGu <kentgu2010@gmail.com>
2018-05-09 21:52:21 +0800

    initial content
    first pull

---
+++ app/src/main/java/kent/com/pojo/CreditCard.java
@@ -0,0 +1,11 @@
+package kent.com.pojo;
+
+
+public class CreditCard {
+    private String cardName;
+    private short cardNum;
+    private int dayOfMonth_bill;
+    private int dueDate;
+    private boolean isFixDueDate;
+
+}
